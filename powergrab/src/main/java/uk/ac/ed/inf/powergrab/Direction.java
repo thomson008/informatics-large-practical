a7816da6bd675f631511e09e7c391cb2cc730078
thomson008 <s1758499@ed.ac.uk>
2019-09-21 18:16:26 +0100

    Implemented nextPosition() and isPlayArea() methods

---
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -0,0 +1,8 @@
+package uk.ac.ed.inf.powergrab;
+
+public enum Direction {
+	E, ENE, NE, NNE,
+	N, NNW, NW, WNW, 
+	W, WSW, SW, SSW,
+	S, SSE, SE, ESE
+}
---
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -0,0 +1,46 @@
+package uk.ac.ed.inf.powergrab;
+
+import java.util.Arrays;
+
+public class Position {
+	public double latitude;
+	public double longitude;
+	
+	/**
+	 * Constructor. Initiates the Position object with geographical coordinates
+	 * @param latitude Geographical latitude of the point on the map
+	 * @param longitude  Geographical longitude of the point on the map
+	 */
+	public Position(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	/**
+	 * Method to compute the new coordinates of the drone after it makes a move
+	 * @param direction One of the 16 wind directions
+	 * @return Position object representing the position of the drone after one move in the direction passed as argument
+	 */
+	public Position nextPosition(Direction direction) {
+		int directionIndex = Arrays.asList(Direction.values()).indexOf(direction);
+		
+		double degreeAngle = directionIndex * 22.5;
+		double radianAngle = Math.toRadians(degreeAngle);
+		
+		double deltaLat = 0.0003 * Math.sin(radianAngle);
+		double deltaLong = 0.0003 * Math.cos(radianAngle);
+		
+		Position newPosition = new Position(this.latitude + deltaLat, this.longitude + deltaLong);
+		
+		return newPosition;
+	}
+	 /**
+	  * Method to check if a move is allowed, i.e. is the drone going to remain within permitted area
+	  * @return boolean, true if drone is within the play area, false if it is outside
+	  */
+	public boolean inPlayArea() { 
+		boolean latitudeOK = this.latitude <= 55.946233 && this.latitude >= 55.942617;
+		boolean longitudeOK = this.longitude <= -3.194319 && this.latitude >= -3.192473;
+		return latitudeOK && longitudeOK;
+	}
+}
