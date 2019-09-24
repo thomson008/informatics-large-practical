5d2d3076b541013581290f0ffa79477a637b6933
thomson008 <s1758499@ed.ac.uk>
2019-09-24 20:53:27 +0100

    simplified url generation

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,6 +1,8 @@
 package uk.ac.ed.inf.powergrab;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -13,12 +15,8 @@ public class App {
 	
     public static void main(String[] args) throws IOException {
     	String JsonURL = "http://homepages.inf.ed.ac.uk/stg/powergrab/";
-    	
-    	for (int i = 0; i < 3; i++) {
-    		JsonURL += args[i] + "/";
-    	}
-    	
-    	JsonURL += "powergrabmap.geojson";
+    	Path path = Paths.get(args[2], args[1], args[0], "powergrabmap.geojson");
+    	JsonURL += path.toString().replace("\\", "/");
         JSONparser.parseJSON(JsonURL);
     }
 }
