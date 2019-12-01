c24fa8ff19a8827e7681fc46446ae6ebe0ccb7ab
thomson008 <s1758499@ed.ac.uk>
2019-12-01 14:13:11 +0000

    blahblah

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/StatelessDrone.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/StatelessDrone.java
@@ -22,29 +22,43 @@ public class StatelessDrone extends Drone {
 	public Direction computeNextMove() {
 		Station posStationWithinMove = getPosStationWithinMove();
 		Direction direction;
-		int directionIdx;
-		Position nextPosition;
 
-		if (posStationWithinMove == null || isWithinDistance(posStationWithinMove)) {			
-			do {
-				directionIdx = random.nextInt(16);
-				direction = Direction.values()[directionIdx];
-				nextPosition = position.nextPosition(direction);
-				// Continue looking for directions that will not put the drone in negative area,
-				// unless there is no such direction
-			} while (!nextPosition.inPlayArea() || nextPosition.inNegativeRange() && !isSurrounded());
-		}
+
+		if (posStationWithinMove == null || isWithinDistance(posStationWithinMove)) 			
+			direction = generateRandomDirection();
 		
 		else  {
 			direction = finalDirection(posStationWithinMove);
 			if (direction == null)
 				direction = position.computeDirection(posStationWithinMove.coordinates);
+			if (!position.nextPosition(direction).getClosest().isPositive())
+				direction = generateRandomDirection();
 		}
 
 		return direction;
 	}
 	
-	
+	/**
+	 * Generates random direction that will not put the drone in a exchange range with a negative station
+	 * (if that is possible)
+	 * @return
+	 */
+	private Direction generateRandomDirection() {
+		Position nextPosition;
+		int directionIdx;
+		Direction direction;
+		
+		do {
+			directionIdx = random.nextInt(16);
+			direction = Direction.values()[directionIdx];
+			nextPosition = position.nextPosition(direction);
+			// Continue looking for directions that will not put the drone in negative area,
+			// unless there is no such direction
+		} while (!nextPosition.inPlayArea() || nextPosition.inNegativeRange() && !isSurrounded());
+		
+		return direction;
+	}
+
 	/**
 	 * Checks if there is any move that will not put the drone in a negative area
 	 * @return
